ff624eb40541fa26929468541f6c1508c7fed5e9
asmadotgh <asma_gh@mit.edu>
2019-03-06 19:23:54 -0500

    helper function for num classes

--- src/my_constants.py
+++ src/my_constants.py
@@ -6,3 +6,7 @@ class Constants:
     @staticmethod
     def get_label_cols(self):
         return ['neutral', 'happiness', 'surprise', 'sadness', 'anger', 'disgust', 'fear', 'contempt', 'unknown', 'NF']
+
+    @staticmethod
+    def get_no_emotions():
+        return len(Constants.get_emotion_cols())
