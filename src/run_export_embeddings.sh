3d52d269f113e34ff25b6ec0714f804c1afc0a4e
asmadotgh <asma_gh@mit.edu>
2019-04-15 12:23:57 -0400

    automating running jobs

--- src/run_export_embeddings.sh
+++ src/run_export_embeddings.sh
@@ -6,14 +6,15 @@ embeddings='Mixed_8b Mixed_8a Mixed_7a Mixed_6b Mixed_5a'
 subset_list=( $subsets )
 embedding_list=( $embeddings )
 
-num_jobs=${#embedding_list[@]}
+num_subsets=${#subset_list[@]}
+num_embeddings=${#embedding_list[@]}
 
 gpu_id=0
 for subset in $subsets
 do
     for embedding in $embeddings
     do
-        if (($gpu_id == num_jobs))
+        if (($gpu_id == $((num_subsets*num_embeddings-1)) ))
         then
             CUDA_VISIBLE_DEVICES=$(($gpu_id % 4)) python export_embeddings.py --embedding_name="$embedding" --data_dir="/mas/u/asma_gh/uncnet/datasets/FER+/$subset.csv"
         else
