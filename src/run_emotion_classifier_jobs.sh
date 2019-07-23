12cb3ddf19536b650d2f2ca982499034f14dd37c
asmadotgh <asma_gh@mit.edu>
2019-07-23 19:26:58 -0400

    running jobs

--- src/run_emotion_classifier_jobs.sh
+++ src/run_emotion_classifier_jobs.sh
@@ -2,18 +2,25 @@
 
 embedding_models='VGGFace2_Inception_ResNet_v1 CASIA_WebFace_Inception_ResNet_v1'
 embedding_layers='Mixed_8b Mixed_8a Mixed_7a Mixed_6b Mixed_6a Mixed_5a'
+available_gpu_list=(0 1 2 3 4 5 6 7)
 
-# running
+# Done
 # matlaber 11
-embedding_models='CASIA_WebFace_Inception_ResNet_v1'
-embedding_layers='Mixed_8b Mixed_8a Mixed_7a Mixed_6b'
-available_gpu_list=(0 1 2 3)
+#embedding_models='CASIA_WebFace_Inception_ResNet_v1'
+#embedding_layers='Mixed_8b Mixed_8a Mixed_7a Mixed_6b'
+#available_gpu_list=(0 1 2 3)
 
 # matlaber 10
 #embedding_models='CASIA_WebFace_Inception_ResNet_v1'
 #embedding_layers='Mixed_6a Mixed_5a'
 #available_gpu_list=(2 3)
+
+#running
+# matlaber 10
 # CUDA_VISIBLE_DEVICES=2 python emotion_classifier.py --embedding_model='CASIA_WebFace_Inception_ResNet_v1' --embedding_layer='Mixed_7a' --single_label --uncertainty_type='epistemic'
+
+# matlaber 6
+# CUDA_VISIBLE_DEVICES=3 python emotion_classifier.py --embedding_model='CASIA_WebFace_Inception_ResNet_v1' --embedding_layer='Mixed_7a' --single_label --uncertainty_type='epistemic' --learning_rate=0.00001
 
 embedding_model_list=( $embedding_models )
 embedding_layer_list=( $embedding_layers )
